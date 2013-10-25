a4fa94bfcec93795e8b73aba8b4482b4f39db1ac
Martin DENIZET <martin.denizet@supinfo.com>
2013-10-25 14:14:15 +0800

    Added a "Delete all records" button. refs #2

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ RedmineApp::Application.routes.draw do
   #match 'hr/:project_id/boards/:board_id/manage', :to => 'boards_watchers#manage', :via => [:get, :post]
 
   match 'login_audit/', :controller => 'login_audit', :action => 'index', :via => [:get]
-  match 'login_audit/purge', :controller => 'login_audit', :action => 'purge', :via => [:post]
+  match 'login_audit/delete', :controller => 'login_audit', :action => 'delete', :via => [:post]
+  match 'login_audit/delete_all', :controller => 'login_audit', :action => 'delete_all', :via => [:post]
 end
