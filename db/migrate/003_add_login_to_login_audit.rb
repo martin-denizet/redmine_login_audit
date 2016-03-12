6e7a743af46abd3cc4d30a4232adbbe3fdf302f2
Martin DENIZET <martin.denizet@supinfo.com>
2016-03-12 16:43:48 +0100

    Make migrations reversible.

--- db/migrate/003_add_login_to_login_audit.rb
+++ db/migrate/003_add_login_to_login_audit.rb
@@ -28,6 +28,6 @@ class AddLoginToLoginAudit < ActiveRecord::Migration
     add_column :login_audits, :api, :boolean, default: false
     add_column :login_audits, :url, :string, limit: 155
     add_column :login_audits, :method, :string, limit: 6
-    remove_column :login_audits, :client
+    remove_column :login_audits, :client, :string
   end
 end
