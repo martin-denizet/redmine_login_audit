fbc7ef7ef966ea9d63af6b6c34697a4685e94a84
Martin DENIZET <martin.denizet@supinfo.com>
2018-01-23 00:03:21 +0100

    Version bump to 0.3.2

--- init.rb
+++ init.rb
@@ -54,7 +54,7 @@ Redmine::Plugin.register :redmine_login_audit do
   name 'Redmine Login Audit plugin'
   author 'Martin DENIZET'
   description 'Login Audit logs login attempts. Can be configured to send emails'
-  version '0.3.1'
+  version '0.3.2'
   url 'https://github.com/martin-denizet/redmine_login_audit'
   author_url 'http://martin-denizet.com'
 
